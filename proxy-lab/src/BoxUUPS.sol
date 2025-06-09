c8389286e17cfcf88290f665bf56cc7ab78d4102
achraf <bradjiachraf01@gmail.com>
2025-06-09 09:02:12 +0100

    added a proxy project to dive into upgradable contracts

---
+++ proxy-lab/src/BoxUUPS.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.27;
+
+import "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
+import "../lib/openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
+import "../lib/openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
+
+contract BoxUUPS is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 public value;
+
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+    }
+
+    function setValue(uint256 _value) external {
+        value = _value;
+    }
+
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
+}
